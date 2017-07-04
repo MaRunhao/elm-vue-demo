b4c6a178064119b98df255407f7dd0aff5b3666b
MaRunhao <78509759@qq.com>
2017-07-04 19:03:38 +0800

    vue init

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 2611,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item"><router-link tag="a" to="/goods">商品</router-link></div>
+      <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
+      <div class="tab-item"><router-link to="/seller">商家</router-link></div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from './components/header/header.vue'
+const ERR_OK = 0
+
 export default {
-  name: 'app'
+    name: 'app',
+    components: {
+      'v-header': header
+    },
+    created () {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.data
+        if (response.errno === ERR_OK) {
+          this.seller = response.data
+          console.log(this.seller)
+        }
+      }, (response) => {
+
+      })
+    },
+    data () {
+      return {
+        seller: {}
+      }
+    }
 }
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +48,28 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#app
+ .tab
+    display: flex
+    position: relative
+    width: 100%
+    height: 40px
+    line-height: 40px
+    &::after
+        position: absolute
+        display: block
+        width: 100%
+        bottom: 0
+        border-bottom: 1px solid rgba(0, 20, 20, .5)
+        content: ' '
+    .tab-item
+        flex: 1
+        text-align: center
+        & > a
+            display: block
+            text-decoration: none
+            font-size: 14px
+            color: rgb(77, 85, 93)
+            &.router-link-active
+                color: rgb(240, 20, 20)
 </style>
